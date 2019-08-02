7d70eccc5077124816ee215588c3e55fed1a09e8
Luis Philipe Fidelis <luizsale10@gmail.com>
2019-08-01 23:36:17 -0300

    Add game board from react tutorial. Passing props to board Square component

--- src/game/Game.test.js
+++ src/game/Game.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Game from './Login';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<LoGamegin />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
